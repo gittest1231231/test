3a0df9a15e9009d2ce895028e4121190e1ff7a5e
wangzxc <15232514223@163.com>
2016-11-30 09:08:28 +0800

    asdas

--- app/src/main/java/bw/test/MainActivity.java
+++ app/src/main/java/bw/test/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i("MainActivity","12321");
+        Log.d("","");
 
     }
 }
